48f0dc932fb9053410839a84e31c7444a05a876c
Bence Hervay <hervay.bence@gmail.com>
2025-12-09 02:15:52 +0000

    D2/1 done

---
+++ day_2/part1.rs
@@ -0,0 +1,27 @@
+fn is_valid(id: u64) -> bool {
+    let id_str = id.to_string();
+    let len = id_str.len();
+    if len % 2 != 0 {
+        return true;
+    }
+    let mid = len / 2;
+    return id_str[..mid] != id_str[mid..];
+}
+
+
+fn main() {
+    let input: String = std::fs::read_to_string("input.txt").unwrap();
+    let ranges: Vec<&str> = input.split(",").collect();
+    let mut invalid: Vec<u64> = Vec::new();
+    for range in ranges {
+        let (start, end): (&str, &str) = range.split_once("-").unwrap();
+        let start: u64 = start.parse().unwrap();
+        let end: u64 = end.parse().unwrap();
+        for id in start..=end {
+            if !is_valid(id) {
+                invalid.push(id);
+            }
+        }
+    }
+    println!("{}", invalid.iter().sum::<u64>());
+}
