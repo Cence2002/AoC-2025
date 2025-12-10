8dcf16a56e198cbfabe37a0d931afb75ab4950a9
Bence Hervay <hervay.bence@gmail.com>
2025-12-10 21:40:26 +0000

    D3/1 done

---
+++ day_3/part_1.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let input: String = std::fs::read_to_string("example.txt").unwrap();
+    let lines: Vec<&str> = input.lines().collect();
+    let mut total_joltage: u32 = 0;
+    for line in lines {
+        let bank: Vec<u8> = line.chars().map(|c| c.to_digit(10).unwrap() as u8).collect();
+        let mut max_joltage: u8 = 0;
+        for i in 0..bank.len() {
+            for j in i+1..bank.len() {
+                let joltage = bank[i] * 10 + bank[j];
+                max_joltage = max_joltage.max(joltage as u32);
+            }
+        }
+        total_joltage += max_joltage;
+    }
+    println!("{}", total_joltage);
+}
