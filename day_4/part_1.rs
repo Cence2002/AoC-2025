933349ae614f223a3859b6c52973a3d0a4457b10
Bence Hervay <hervay.bence@gmail.com>
2025-12-10 23:30:17 +0000

    D4/1 done

---
+++ day_4/part_1.rs
@@ -0,0 +1,38 @@
+const INPUT_PATH: &str = "input.txt";
+
+fn main() {
+    let input: String = std::fs::read_to_string(INPUT_PATH).unwrap();
+    let lines: Vec<&str> = input.lines().collect();
+    let grid_size = lines.len();
+    let mut roll_grid: Vec<Vec<bool>> = Vec::new();
+    for line in lines {
+        roll_grid.push(line.chars().map(|c| c == '@').collect::<Vec<bool>>());
+    }
+    let mut accessible: u32 = 0;
+    for x in 0 .. grid_size as isize {
+        for y in 0 .. grid_size as isize {
+            if !roll_grid[x as usize][y as usize] {
+                continue;
+            }
+            let mut neighbours: u8 = 0;
+            for (dx, dy) in [
+                (1, 0), (-1, 0), (0, 1), (0, -1),
+                (1, 1), (1, -1), (-1, 1), (-1, -1),
+            ] {
+                let neighbour_x = x + dx;
+                let neighbour_y = y + dy;
+                if neighbour_x < 0 ||
+                    neighbour_y < 0 ||
+                    neighbour_x >= grid_size as isize ||
+                    neighbour_y >= grid_size as isize {
+                    continue;
+                }
+                neighbours += roll_grid[neighbour_x as usize][neighbour_y as usize] as u8;
+            }
+            if neighbours < 4 {
+                accessible += 1;
+            }
+        }
+    }
+    println!("{}", accessible);
+}
